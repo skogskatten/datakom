41967208cb57de1fe7ae3468879fbe024c24129c
Katten <83346239+skogskatten@users.noreply.github.com>
2021-05-24 18:21:18 +0200

    Update server.c
    moved bind to server.c from functions.c

--- lab3/server.c
+++ lab3/server.c
@@ -10,10 +10,18 @@
 int main(void)
 {
     int listen_sock;
+    struct sockaddr_in server_addr;
     
     /* Create and initialize socket */
     printf("Initializing server.\n");
-    listen_sock = makeSocket(PORT_NUM+45000);
+    listen_sock = makeSocket(PORT_NUM, &server_addr);
+    
+    /* Assign address to socket */
+    if(bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
     
     /* Main program loop */
     printf("Initialized, waiting for connections.\n");
@@ -33,4 +41,3 @@ int main(void)
     
     return 0;
 }
-

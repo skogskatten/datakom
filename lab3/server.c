e25370e97e368c643cc3999bd6c3c4fac8741820
Katten <83346239+skogskatten@users.noreply.github.com>
2021-05-25 13:53:06 +0200

    Update server.c
    changed structure of case psuedo code

--- lab3/server.c
+++ lab3/server.c
@@ -9,66 +9,66 @@
 
 int main(void)
 {
-  int sock, state;
-  struct sockaddr_in server_addr;
-  fd_set sock_set, read_set, write_set;
+    int sock, state;
+    struct sockaddr_in server_addr;
+    fd_set sock_set, read_set, write_set;
     
-  /* Create and initialize socket */
-  printf("Initializing server.\n");
-  sock = makeSocket(PORT_NUM, &server_addr);
+    /* Create and initialize socket */
+    printf("Initializing server.\n");
+    sock = makeSocket(PORT_NUM, &server_addr);
     
-  /* Assign address to socket */
-  if(bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+    /* Assign address to socket */
+    if(bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
     {
-      perror("bind");
-      exit(EXIT_FAILURE);
+        perror("bind");
+        exit(EXIT_FAILURE);
     }
 
-  /* Initialize sock_set to a null (empty) set */
-  FD_ZERO(&sock_set);
-  /* Add sock to the sock_set */
-  FD_SET(sock, &sock_set);
+    /* Initialize sock_set to a null (empty) set */
+    FD_ZERO(&sock_set);
+    /* Add sock to the sock_set */
+    FD_SET(sock, &sock_set);
     
-  /* Main program loop */
-  printf("Initialized, waiting for connections.\n");
-  while(1) {
-      
-    read_set = sock_set;
-      
-    write_set = sock_set;
+    /* Main program loop */
+    printf("Initialized, waiting for connections.\n");
+    while(1)
+    {
+        read_set = sock_set;
+        write_set = sock_set;
     
-    /* Något händer, avgörs med select */
+        /* Något händer, avgörs med select */
 
-    /*nedan följer pseudokod med förslag på tillståndsflöde. */
-    switch(state) {
-      
-    case(INIT):/* INIT: Initial state, receives packets */
-      receive;
-      if(order)
-	switch(FLAG) {
-	case(SYN):
-	  break;
-      	case(FIN):
-	  break;
-	case(ACK):
-	  break;
-	case(DATA):
-	  break;
-	}
-      else
-	/* resend last ack */;
-      break;
-      
-    case(WAIT_ACK):
-      break;
-
-    case(WAIT_SYN-ACK):
-      break;
-
-    } //switch end
+        /*nedan följer pseudokod med förslag på tillståndsflöde. */
+        switch(mode)
+        {
+        case(AWAIT_CONNECT):
+            
+	        break;
+        case(CONNECTED):
+            switch(state)
+            {
+            case(RECIEVE): //wait
+                switch(event)
+                {
+                case(PACKAGE_EXPECTED):
+                    //send ack
+                    break;
+                case(PACKAGE_WRONG):
+                    //send prev ack
+                    break;
+                case(PACKAGE_FIN):
+                    //switch mode teardown
+                    break;
+                }
+            }
+            break;
+        case(TEARDOWN):
+            break;
+        default;
+            //no op
+        }
+    }
     
-  } //main while end
-
-  //  unlink((struct sockaddr *) &server_addr);
-  return 0;
+    //  unlink((struct sockaddr *) &server_addr);
+    return 0;
 }

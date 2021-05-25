6ff3e3f900c82e987a07cf2d5477b489984c9538
birjergarl <ark19001@student.mdh.se>
2021-05-25 18:42:49 +0200

    Skrivit pseudokod for klientloop. wip.

--- lab3/client.c
+++ lab3/client.c
@@ -29,7 +29,68 @@ int main(void)
     
     /* Main program loop */
     printf("Initialized, sending messages.\n");
- 
+	while(1)
+	  {
+		switch(mode)
+		  {
+			
+		  case(CONNECT):			
+			switch(state)
+			  {
+			  case(WANTS_TO_CONNECT):
+				//send SYN
+				//state = WAIT
+				break;
+				
+			  case(WAIT):
+				// receives SYN-ACK / Send ACK AND connect
+				// OR timeout / state = WANTS_TO_CONNECT
+				// state = connected -> mode = SENDING
+				break;
+				
+			  }
+			
+		  case(SENDING):
+			switch(event)
+			  {
+			  case(ACK):
+				// if ACK expected / move window, reset timeout
+				// else if ACK ! expected / resend from ACK sequence number
+				break;
+
+			  case(NO_ACK):
+				// if window not full / send next packet, start timer
+				// else if timeout / resend from last and including N (?)
+				// else Window full / do nothing
+				break;
+			  }
+			break;
+
+		  case(WANTS_TO_CLOSE):
+			switch(state)
+			  {
+			  case(CONNECTED):
+				// Shut down (wait for acks?) / Send FIN
+				// state = WAITING_FOR_ACK
+				break;
+			  case(WAITING_FOR_ACK):
+				// Timeout / - -> state = TIMEOUT OR
+				// ACK received / - -> state = WAITING_FOR_FIN (from server) OR
+				// FIN received / send FIN-ACK -> state = WAITING_ACK_2(?*)
+				/* *Lägg ev till en variabel som talar om ifall  klient har mottagit en FIN eller ej. Då kan
+				  en återanvända samma WAITING_FOR_ACK-tillstånd */
+				break;
+			  case(WAITING_FOR_FIN):
+				// Timeout / - -> state = TIMEOUT OR
+				// FIN received / send FIN-ACK -> state = Timeout
+				break;
+			  case(TIMEOUT):
+				//timeout / Close connection -> state = disconnected
+				break;
+				
+			  }
+		  }
+	  }
     char message[] = {"HELLO"};
                 
     sendto(sock, message, strlen(message), 0,

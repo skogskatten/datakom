61f15475908fe09d32a2615f465bc189e0adc330
Alfred <84722970+birjergarl@users.noreply.github.com>
2022-04-01 14:27:37 +0200

    laddar upp initiala filer igen
    ersätter server och client

--- lab3/client.c
+++ lab3/client.c
@@ -1,111 +1,237 @@
-/*************************************************
- * File: client.c                                *
- * Name: Client program with UDP                 *
- * Authors: agn, ark                             *
- * Purpose: lab3                                 *
- * ***********************************************/
+/* #include<stdio.h> */
+/* #include<stdlib.h> */
+/* #include<unistd.h> */
+/* #include<string.h> */
+/* #include<sys/time.h> */
+/* #include<sys/types.h> */
+/* #include<sys/socket.h> // struct sockaddr */
+/* #include<arpa/inet.h> */
+/* #include<netinet/in.h> // För struct sockaddr_in */
+/* #include<netdb.h> */
+#include "functions.h"
 
-#include "functions.c"
+#define PORT 5555
+#define MAXLINE 1024
+#define hostNameLength 50
 
-int main(void)
-{
-    int sock;
-    struct sockaddr_in server_addr;
-    
-    /* Create and initialize socket */
-    printf("Initializing client.\n");
-    sock = makeSocket(PORT_NUM, &server_addr);
-    
-    /* Connect to address */
-    /* "If the socket sockfd is of type SOCK_DGRAM, then addr is the
-     * address to which datagrams are sent by default, and the only
-     * address from which datagrams are received.
-     */
-    if(connect(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
-    {
-        perror("connect");
-        exit(EXIT_FAILURE);
+#define RESET -1
+
+#define UNCONNECTED 0
+#define CONNECTED 1
+#define TEARDOWN 2
+
+#define WANTS_TO_CONNECT 0
+#define WAIT_FOR_SYNACK 1
+#define SYNACK_RECEIVED 2
+
+#define FIN_SENT_WAIT_ACK 0
+#define ACK_RECVD_WAIT_FIN 1
+#define FIN_RECVD_WAIT_ACK 2
+#define TIMEOUT 3
+#define DISCONNECTED 4
+
+int main(int argc, char *argv[]) {
+  int sockfd, retval, mode, state, n;
+  char buffer[MAXLINE];
+  char hostName[hostNameLength];
+  char *msg;
+  struct sockaddr_in s_addr,c_addr;
+  fd_set read_fd, write_fd, active_fd;
+  FD_ZERO(&read_fd);
+  FD_ZERO(&write_fd);
+  FD_ZERO(&active_fd);
+
+  struct timeval read_timeout;
+  read_timeout.tv_sec = 1;
+  read_timeout.tv_usec = 0;
+  
+  
+  /* Check arguments */
+  if(argv[1] == NULL) {
+    perror("Usage: client [host name]\n");
+    exit(EXIT_FAILURE);
+  }
+  else {
+    strncpy(hostName, argv[1], hostNameLength);
+    hostName[hostNameLength - 1] = '\0';
+  }
+  
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if(sockfd < 0) {
+    perror("Exit, server socket creation failure");
+    exit(EXIT_FAILURE);
+  }
+
+  /* if(setsockopt(sockfd, SOL_SOCKET, SO_RECVTIMEO, (const void *)&read_timeout, sizeof(read_timeout)) < 0) { */
+  /*   perror("Client, setting sockopt"); */
+  /*   exit(EXIT_FAILURE); */
+  /* } */
+  
+  int s_addrlen = sizeof(s_addr), c_addrlen = sizeof(c_addr);
+  
+  memset(&buffer, 0, MAXLINE);
+  memset(&s_addr, 0, s_addrlen);
+  
+  struct hostent *hostInfo; /* Contains info about the host */
+  hostInfo = gethostbyname(hostName);
+  if(hostInfo == NULL) {
+    fprintf(stderr, "initSocketAddress - Unknown host %s\n",hostName);
+    exit(EXIT_FAILURE);
+  }
+  
+  s_addr.sin_addr = *(struct in_addr *)hostInfo->h_addr;
+  s_addr.sin_family = AF_INET;
+  s_addr.sin_port = htons(PORT);
+
+  mode = UNCONNECTED;
+  state = WANTS_TO_CONNECT;
+  n = 0;
+  switch(mode) {
+  default :
+    printf("Undefined case: mode . Exiting.\n");
+    exit(EXIT_FAILURE);
+
+  case UNCONNECTED :
+    printf("MODE: unconnected.\n");
+
+    switch(state) {
+    default :
+      printf("Undefined case: state . Exiting.\n");
+      exit(EXIT_FAILURE);
+      
+    case WANTS_TO_CONNECT :
+      printf("STATE: wants to connect.\n");
+      msg = "syn";
+      if(sendto(sockfd, (const void *)msg, strlen(msg), 0, (const struct sockaddr *)&s_addr, s_addrlen) < 0){
+	perror("Sendto");
+	exit(EXIT_FAILURE);
+      }
+      printf("Msg sent.\n");
+      state = WAIT_FOR_SYNACK;
+      
+    case WAIT_FOR_SYNACK :
+      printf("STATE: wait for syn-ack.\n");
+      //lägg till timeout, select med timeval
+      usleep(5000);
+      if(retval = recvfrom(sockfd, (void *)buffer, MAXLINE, 0, (struct sockaddr *)&s_addr, &s_addrlen) < 0) {
+	perror("Client, receiving syn-ack");
+	exit(EXIT_FAILURE);
+      }
+      printf("Client, listening for syn-ack: received %d bytes from %s.\n", retval, inet_ntoa(s_addr.sin_addr));
+      printf("Msg: %s\n", buffer);
+      memset(&buffer, '\0', MAXLINE);
+      state = SYNACK_RECEIVED;
+
+    case SYNACK_RECEIVED :
+      printf("STATE: syn-ack received.\n");
+      /* går direkt från att skicka syn-ack-ack till att koppla upp, men om syn-ack-acken försvinner ansluter klienten men servern stänger buffern. */
+      /* lös ovan med att koppla ned om sändning inte fungerar sedan, timeout i connected */
+      msg = "syn-ack-ack";
+      if(sendto(sockfd, (void *)msg, strlen(msg), 0,(struct sockaddr *)&s_addr, s_addrlen) < 0) {
+	perror("Client, sending syn-ack-ack");
+	exit(EXIT_FAILURE);
+      }
+      mode = CONNECTED;
     }
     
-    /* Main program loop */
-    printf("Initialized, sending messages.\n");
-	while(1)
-	  {
-		switch(mode)
-		  {
-			
-		  case(CONNECT):
-			switch(state)
-			  {
-			  case(WANTS_TO_CONNECT):
-				//send SYN / - -> state = WAIT
-				break;
-				
-			  case(WAIT):
-				switch(event)
-				  {					
-				  case(SYN-ACK):
-					// receives SYN-ACK / Send ACK AND connect -> mode = SENDING
-					break;
-				  case(TIMEOUT):
-					// timeout / - -> state = WANTS_TO_CONNECT					
-					break;
-				  }
-				break;
-			  }
-			
-		  case(SENDING):
-			switch(event)
-			  {
-			  case(ACK): /* Skall vi kontrollera om korrekt N här eller innan switch? */
-				// if ACK expected / move window, reset timeout
-				// else if ACK ! expected / resend from ACK sequence number
-				break;
-				
-			  case(NO_ACK): /* If-satser eller ytterliggare switch-case? Enklare med några if i det här fallet? */
-				// if window not full / send next packet, start timer
-				// else if timeout / resend from last and including N (?)
-				// else Window full / do nothing
-				break;
-			  }
-			break;
+  case CONNECTED :
+    printf("MODE: CONNECTED.\n");
+    
+    msg = "Select nr.";
+    if(sendto(sockfd, (void *)msg, strlen(msg), 0,(struct sockaddr *)&s_addr, s_addrlen) < 0) {
+      perror("Client, sending first select msg");
+      exit(EXIT_FAILURE);
+    } //Lägg till timeout ifall syn-ack-ack kommer bort
+    if(retval = recvfrom(sockfd, (void *)buffer, MAXLINE, 0, (struct sockaddr *)&s_addr, &s_addrlen) < 0) {
+      perror("Client, receiving msg ack");
+      exit(EXIT_FAILURE);
+    }
+    //    printf("Received msg %d.\n",(int)buffer); DEBUG
+    printf("Received msg %s.\n", buffer);
+    memset(&buffer, '\0', MAXLINE);
 
-		  case(WANTS_TO_CLOSE):
-			switch(state)
-			  {
-			  case(CONNECTED):
-				// Shut down (wait for acks?) / Send FIN -> state = WAITING_FOR_ACK
-				break;
-					
-			  case(WAITING_FOR_ACK):
-				switch(event)
-				  {
-				  case(timeout):
-					// Timeout / - -> state = TIMEOUT OR
-					break;
-						
-				  case(ACK):
-					// ACK received / - -> state = WAITING_FOR_FIN (from server) OR
-					break;
 
-				  case(FIN):
-					// FIN received / send FIN-ACK -> state = WAITING_ACK_2(?*)
-					break;
-					/* *Lägg till en variabel som talar om ifall  klient har mottagit en FIN eller ej. Då kan
-					   en återanvända samma WAITING_FOR_ACK-tillstånd */
-				  }
-				break;
-					
-			  case(WAITING_FOR_FIN):
-				// Timeout / - -> state = TIMEOUT OR
-				// FIN received / send FIN-ACK -> state = Timeout
-				break;
-			  case(TIMEOUT):
-				//timeout / Close connection -> state = disconnected
-				break;
-			  }
-		  }
-	  }
-	
-    return 0;
+    /* Just nu faller den igenom alla TIMEOUT. behöver while loop alt. ex if-else för att skippa fall då gått till timeout */  
+  case TEARDOWN :
+    printf("MODE: TEARDOWN.\n");
+
+    msg = "FIN";
+    if(sendto(sockfd, (void *)msg, strlen(msg), 0,(struct sockaddr *)&s_addr, s_addrlen) < 0) {
+      perror("Client, sending first select msg");
+      exit(EXIT_FAILURE);
+    }
+
+    state = FIN_SENT_WAIT_ACK;
+    switch(state) {
+    case FIN_SENT_WAIT_ACK :
+      printf("STATE: FIN sent, awaiting ack.\n");
+      read_fd = active_fd;
+      retval = select(sockfd + 1, &read_fd, NULL, NULL, &read_timeout);
+      switch(retval) {
+      case -1 :
+	perror("Client, select");
+	exit(EXIT_FAILURE);
+      case 0 :
+	printf("Timeout, going to timeout state.\n");
+	state = TIMEOUT;
+	break;
+      default :
+	if(recvfrom(sockfd, (void *)buffer, MAXLINE, 0, (struct sockaddr *)&s_addr, &s_addrlen) < 0) {
+	  perror("Client, receiving fin-ack");
+	  exit(EXIT_FAILURE);
+	}
+	printf("Received fin-ack.\n");
+	state = ACK_RECVD_WAIT_FIN;
+      }
+
+    case ACK_RECVD_WAIT_FIN :
+      printf("STATE: FIN-ACK received, waiting for FIN.\n");
+      read_fd = active_fd;
+      retval = select(sockfd + 1, &read_fd, NULL, NULL, &read_timeout);
+      switch(retval) {
+      case -1 :
+	perror("Client, select");
+	exit(EXIT_FAILURE);
+      case 0 :
+	printf("Timeout, going to timeout state.\n");
+	state = TIMEOUT;
+	break;
+      default :
+	if(recvfrom(sockfd, (void *)buffer, MAXLINE, 0, (struct sockaddr *)&s_addr, &s_addrlen) < 0) {
+	  perror("Client, receiving fin");
+	  exit(EXIT_FAILURE);
+	}
+	printf("Received fin.\n");
+	state = FIN_RECVD_WAIT_ACK;
+      }
+      
+    case FIN_RECVD_WAIT_ACK :
+      printf("STATE: FIN sent and received, waiting for FIN-ACK.\n");
+      read_fd = active_fd;
+      retval = select(sockfd + 1, &read_fd, NULL, NULL, &read_timeout);
+      switch(retval) {
+      case -1 :
+	perror("Client, select");
+	exit(EXIT_FAILURE);
+      case 0 :
+	printf("Timeout, going to timeout state.\n");
+	state = TIMEOUT;
+	break;
+      default :
+	if(recvfrom(sockfd, (void *)buffer, MAXLINE, 0, (struct sockaddr *)&s_addr, &s_addrlen) < 0) {
+	  perror("Client, receiving fin-ack");
+	  exit(EXIT_FAILURE);
+	}
+	printf("Received fin-ack.\n");
+	state = TIMEOUT;
+      }
+    case TIMEOUT :
+      usleep(500000);
+      close(sockfd);
+    }
+  }
+
+  usleep(50000);
+  exit(EXIT_SUCCESS);
+
 }
--- lab3/server.c
+++ lab3/server.c
@@ -1,156 +1,199 @@
-/*************************************************
- * File: server.c                                *
- * Name: Server program with UDP                 *
- * Authors: agn, ark                             *
- * Purpose:                                      *
- * ***********************************************/
+/* #include<stdio.h> */
+/* #include<stdlib.h> */
+/* #include<unistd.h> */
+/* #include<string.h> */
+/* #include<sys/time.h> */
+/* #include<sys/types.h> */
+/* #include<sys/socket.h> // struct sockaddr */
+/* #include<arpa/inet.h> */
+/* #include<netinet/in.h> // För struct sockaddr_in */
+#include "functions.h"
 
-#include "functions.c"
+#define PORT 5555 // plus en port till för uppkopplad klient? portnummer skickas då vi syn-ack.
+#define MAXLINE 1024
+#define WINDOW_SIZE 5
+//MODE
+#define UNCONNECTED 0
+#define CONNECTED 1
+#define TEARDOWN 2
+//STATE
+//0.
+#define RESET -1
+//1. Awaiting connection
+#define LISTEN 0
+#define LISTEN_WAIT_SYNACKACK 1
+#define LISTEN_ACK_RECEIVED 2
 
-int main(void)
-{
-    int sock, state, i, j;
-    struct sockaddr_in server_addr;
-    fd_set sock_fd_set, read_fd_set, write_fd_set;
 
-    /* Create and initialize socket */
-    printf("Initializing server.\n");
-    sock = makeSocket(PORT_NUM, &server_addr);
+int main(int argc, char *argv[]) {
+  int s_sockfd, c_sockfd, retval, mode, state, n = 0;
+  fd_set active_fd, read_fd;
+  char buffer[MAXLINE];
+  char *msg;
+  struct sockaddr_in s_addr,s_addr2, c_addr, c_addr2;
 
-    /* Assign address to socket */
-    if (bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
-    {
-        perror("bind");
-        exit(EXIT_FAILURE);
+  FD_ZERO(&active_fd);
+  FD_ZERO(&read_fd);
+  memset(&buffer, 0, MAXLINE);
+  memset(&s_addr, 0, sizeof(s_addr));
+  memset(&s_addr2, 0, sizeof(s_addr2));
+  memset(&c_addr, 0, sizeof(c_addr));
+  memset(&c_addr2, 0, sizeof(c_addr2));
+
+  int s_addrlen = sizeof(s_addr), s_addr2len = sizeof(s_addr2),
+    c_addrlen = sizeof(c_addr), c_addr2len = sizeof(c_addr2);
+  
+  s_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if(s_sockfd < 0) {
+    perror("Exit, server socket creation failure");
+    exit(EXIT_FAILURE);
+  }
+  
+  s_addr.sin_family = AF_INET;
+  s_addr.sin_addr.s_addr = INADDR_ANY; // se ip(7)
+  s_addr.sin_port = htons(PORT);
+  
+  if(bind(s_sockfd, (const struct sockaddr *) &s_addr, s_addrlen) < 0){
+    perror("Exit, server socket address binding error");
+    exit(EXIT_FAILURE);
+  }
+
+  mode = UNCONNECTED;
+  state = LISTEN;
+  switch(mode) {
+  default :
+    printf("Undefined case: mode. Exiting.\n");
+    exit(EXIT_FAILURE);
+  case UNCONNECTED :
+    printf("MODE: UNCONNECTED.\n"); // Antar alla inkommande meddelanden är syn
+    if(state == RESET)
+      state = LISTEN;
+    switch(state) {
+    default :
+      printf("Undefined case: unconnected state . Exiting.\n");
+      exit(EXIT_FAILURE);
+
+      
+    case LISTEN :
+      printf("STATE: listening for syn.\n");
+      
+      retval = recvfrom(s_sockfd, (void *)buffer, MAXLINE, 0 /* MSG_DONTWAIT */, (struct sockaddr *)&c_addr, &c_addrlen);
+      if(retval == -1) {
+	perror("Server, listening for connection");
+	break;
+      }
+      else {
+	printf("Server, listening for connection: received %d bytes from %s.\n", retval, inet_ntoa(c_addr.sin_addr));
+	printf("Msg: %s\n", buffer);
+	memset(&buffer, '\0', MAXLINE);
+
+	c_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(c_sockfd < 0) {
+	  perror("Server, creating client socket");
+	  exit(EXIT_FAILURE);
+	}
+
+	msg = "syn-ack";
+	printf("Sending: %s\n", msg);
+	if(sendto(c_sockfd, (const void *)msg, strlen(msg), 0, (const struct sockaddr *)&c_addr, c_addrlen) < 0) {
+	  perror("Server, sending syn-ack");
+	  close(c_sockfd);
+	  break;
+	}
+
+	state = LISTEN_WAIT_SYNACKACK;
+      }
+
+
+    case LISTEN_WAIT_SYNACKACK :
+      printf("STATE: waiting for syn-ack-ack.\n");
+      FD_ZERO(&active_fd);
+      FD_ZERO(&read_fd);
+      FD_SET(c_sockfd, &active_fd);
+      read_fd = active_fd;
+      
+      switch(select(c_sockfd + 1, &read_fd, NULL, NULL, NULL)) {
+      case -1:
+	perror("Server, select c_sockfd");
+	exit(EXIT_FAILURE);
+      case 0:
+	printf("Server, select c_sockfd: nothing to read. Returning to listen state.\n");
+	close(c_sockfd);
+	state = LISTEN;
+	FD_ZERO(&active_fd);
+	break;
+      default:
+	retval = recvfrom(c_sockfd, (void *)buffer, MAXLINE, MSG_DONTWAIT, (struct sockaddr *)&c_addr2, &c_addr2len);
+	if(retval == -1) {
+	  perror("Server, listening for syn-ack-ack");
+	  exit(EXIT_FAILURE);
+	}
+	else {
+	  printf("Server, listening for syn-ack-ack: received %d bytes from %s.\n", retval, inet_ntoa(c_addr2.sin_addr));
+	  printf("Msg: %s\n", buffer);
+	  memset(&buffer, '\0', MAXLINE);
+
+
+	  retval = recv_rtp(c_sockfd, rtp *package, &c_addr2);
+	  state = LISTEN_ACK_RECEIVED;
+	
+      }
+      
+    case LISTEN_ACK_RECEIVED :
+      printf("STATE: syn-ack-ack received.\n");
+      mode = CONNECTED;
+      state = RESET;
+     
     }
+    if(mode != CONNECTED)
+      break;
 
-    /* Initialize sock_fd_set to a null (empty) set */
-    FD_ZERO(&sock_fd_set);
-    /* Add sock to the sock_fd_set */
-    FD_SET(sock, &sock_fd_set);
 
-    /* Main program loop */
-    printf("Initialized, waiting for connections.\n");
-    while (1)
-    {
-        int mode, state, event;
-        read_fd_set = sock_fd_set;
-        write_fd_set = sock_fd_set;
+    
+  case CONNECTED :
+    printf("MODE: CONNECTED.\n");
+    
+    while(mode == CONNECTED) {
+      read_fd = active_fd;
 
-        /* Select selects the sockets ready for input or output.
-         * Not clear if we need write_fd_set here, or at all */
-        if (select(FD_SETSIZE, &read_fd_set, &write_fd_set, NULL, NULL, NULL) < 0)
-        {
-            perror("Select failed\n");
-            exit(EXIT_FAILURE);
-        }
 
-        /* nedan följer pseudokod med förslag på tillståndsflöde.
-         * Fungerar endast för 1 klient? Kanske? Varje ny klient får,
-         * efter connect, en egen socket där meddelanden från klienten
-         * tas emot/skickas(?), vilket gör AWAIT_CONNECT inaktuellt för
-         * de andra socketarna, samtidig som socket 0, serverns "publika"
-         * socket aldrig lämnar uppkoppling. Borde funka bra med en if-sats
-         * som slänger ut alla klientbrevlådor ur AWAIT_CONNECT
-         */
-        
-        for(i = 0; i < FD_SETSIZE; i++)
-        {
-            if(FD_ISSET(i, read_fd_set))
-            {
-                switch(mode)
-                {
-                case (MODE_AWAIT_CONNECT):
-                    //Only sock should ever use this mode
-                    if (curr != sock)
-                    {
-                        mode = CONNECTED;
-                        
-                    }
-                    break;
-                        
-                    switch (state)
-                    {
-                    //Listening
-                    case(STATE_LISTEN):
-                        // EVENT: receives SYN / send SYN-ACK -> state = Wait
-                        break;
+      switch(select(c_sockfd + 1, &read_fd, NULL, NULL, NULL)) {
+      case -1:
+	perror("Server, select c_sockfd");
+	exit(EXIT_FAILURE);
+	
+      case 0:
+	printf("Server, select c_sockfd: nothing to read.\n");
+	break;
+	
+      default:
+	retval = recvfrom(c_sockfd, (void *)buffer, MAXLINE, MSG_DONTWAIT, (struct sockaddr *)&c_addr2, &c_addr2len);
+	if(retval == -1) {
+	  perror("Server, listening for 1st message");
+	}
+	else {
+	  printf("Server, listening for 1st message: received %d bytes from %s.\n", retval, inet_ntoa(c_addr2.sin_addr));
+	  printf("Msg: %s\n", buffer);
+	  memset(&buffer, '\0', MAXLINE);
+	}
+      }
+      
+      if(sendto(c_sockfd, (void *)&n, sizeof(&n), 0, (struct sockaddr *)&c_addr2, c_addr2len) < 0){
+	perror("Server, send msg ack");
+	exit(EXIT_FAILURE);
+      }
 
-                    case(STATE_AWAIT_SYN_ACK):
-                        switch(event)
-                        {
-                        case(STATE_AWAIT_ACK):
-                            // ACK / Connect -> state = Connected
-                            break;
-
-                        case(STATE_TIMEOUT):
-                            // Timeout / - -> state = LISTENING
-                            break;
-                        }
-                        break;
-
-                    case(STATE_CONNECTED):
-                        if (curr == sock)
-                            state = LISTENING;
-                        else {
-                            mode = CONNECTED;
-                            state = RECIEVE;
-                        }
-                        break;
-                    }
-                    break;
-
-                case(MODE_CONNECTED):
-                    switch(state)
-                    {
-                    case(STATE_LISTEN): //wait
-                        switch (event)
-                        {
-                        case (PACKAGE_EXPECTED):
-                            //send ack
-                            break;
-                        case (PACKAGE_WRONG):
-                            //send prev ack
-                            break;
-                        case (PACKAGE_FIN):
-                            // - / Send FIN-ACK -> mode =  TEARDOWN, state = SHUTTING_DOWN
-                            break;
-                        }
-                    }
-                    break;
-
-                case(TEARDOWN):
-                    /* Server has received a FIN */
-                    switch(state)
-                    {
-                    case(STATE_SHUTTING_DOWN):
-                        // Shutting down, preparing to close connection
-                        // Do the needful / Send FIN -> state WAITING_FOR_FIN-ACK
-                        break;
-
-                    case(STATE_AWAIT_FIN_ACK):
-                        switch(event)
-                        {
-                        case (FIN - ACK):
-                            // - / Disconnect -> state = disconnected
-                            break;
-
-                        case (TIMEOUT):
-                            // - / Disconnect -> state = disconnected
-                            break;
-                        default:
-                            // Do nothing
-                            break;
-                        }
-                        break;
-                        // DISCONNECTED
-                    }
-                    default;
-                    //no op
-                } //switch(mode)
-            } //if FD_ISSET
-        } //for FD_SETSIZE
-    } //while
-    //  unlink((struct sockaddr *) &server_addr);
-    return 0;
+      n++;
+    }
+    
+    //    break;
+  
+  case TEARDOWN :
+    printf("MODE: TEARDOWN\n");
+  }
+  
+  usleep(5000);
+  //  close(c_sockfd);
+  close(s_sockfd);
+  exit(EXIT_SUCCESS);
 }
